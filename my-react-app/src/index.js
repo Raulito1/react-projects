07feb413af199b7df8acc1b889f6c6dd82425de6
Raul <ralvaradojr399@gmail.com>
2022-05-20 21:29:22 -0400

    Added faker library and semantic UI

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import faker from 'faker';
 const App = () => {
-   return <div>Hello World!</div>
+   return (
+      <div className='ui container comments'>
+         <div className='comment'>
+            <a href='/' className='avatar'>
+               <img alt='avatar' src={faker.image.image()} />
+            </a>
+            <div className='content'>
+               <a href='/' className='author'>
+                  Sam
+               </a>
+               <div className='metadata'>
+                  <span className='date'>today @ 6pm</span>
+               </div>
+               <div className='text'>Nice blog Post!</div>
+            </div>
+         </div>
+      </div>
+   )
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
